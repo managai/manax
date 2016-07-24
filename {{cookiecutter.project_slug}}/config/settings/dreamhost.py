d66a3b7ad5427bd5ef29e5a1dc18fcc3a4f806b2
Bruno Santeramo <bruno.santeramo@gmail.com>
2016-07-24 15:23:42 +0200

    upgrade

--- {{cookiecutter.project_slug}}/config/settings/dreamhost.py
+++ {{cookiecutter.project_slug}}/config/settings/dreamhost.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
-omport os
+import os
 from .common import *
+
+# load env variables from env.dreamhost
+env.read_env(str(ROOT_DIR) + '/env.dreamhost')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -9,18 +12,9 @@ STATIC_ROOT = os.path.dirname(ROOT_DIR) + '/../public/static/'
 
 SECRET_KEY = 'o}ljke~Rrgg@yGwVIJf~k.(<5E+=5b|0=@(Gb=cO9%Fj~5wi4z'
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = ['{{ cookiecutter.domain_name }}']
 
-# Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': '',
-        'USER': 'osefveni',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
-    }
-}
-
+# DATABASE CONFIGURATION
+# ------------------------------------------------------------------------------
+# Raises ImproperlyConfigured exception if DATABASE_URL not in os.environ
+DATABASES['default'] = env.db('DATABASE_URL')
